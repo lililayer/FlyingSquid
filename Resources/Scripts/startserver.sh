2bfe4c80c1233a52ef8ea59fb9bd32aefdd0c122
Lili <163665831+lililayer@users.noreply.github.com>
2026-07-15 11:45:16 +0200

    Update startserver.sh

--- Resources/Scripts/startserver.sh
+++ Resources/Scripts/startserver.sh
@@ -1,9 +1,9 @@
 cd Resources/ServerDirectory/
 
-echo 'Creating virtual environement (sea)...'
+#echo 'Creating virtual environement (sea)...'
 #python3 -m venv sea
-
-echo 'Starting sea...'
+#echo 'Starting sea...'
+#source ... too busy for now
 
 echo 'starting the server...'
 gnome-terminal  --title='SEA' -- bash -c "python3 -m http.server 27015 --directory ./"
